d129e1fab226970eea763d7f4940058dabaf8b63
Timotej Dzian <timotejdzian@email.com>
2026-05-20 20:41:23 +0200

    add matplotlib surface visualisation

---
+++ bs/visualisation.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.interpolate import griddata
+
+
+def visualize(calls_df, S):
+    calls_df = calls_df.copy()
+    calls_df["moneyness"] = calls_df["strike"] / S
+    filters = ((calls_df["bid"] > 0) 
+              & (calls_df["IV"].between(0.01, 5)) 
+              & (calls_df["T"] >= 5/365) 
+              & (calls_df["moneyness"].between(0.7, 1.3)))
+    t_calls_df = calls_df[filters].copy()
+    
+    x = t_calls_df["moneyness"].values
+    y = t_calls_df["T"].values
+    z = t_calls_df["IV"].values
+
+    # create 2D space of evenly spaced data 
+    xi = np.linspace(x.min(), x.max(), 50)
+    yi = np.linspace(y.min(), y.max(), 50)
+    X, Y = np.meshgrid(xi, yi)
+    # interpolation 
+    Z = griddata((x, y), z, (X, Y), method = "linear")
+
+    #create the graph
+    fig = plt.figure(figsize = (12, 9))
+    ax = fig.add_subplot(111, projection="3d")
+    ax.plot_surface(X, Y, Z, cmap = "viridis", edgecolor = "none", alpha = 0.9)
+    ax.set_xlabel("Moneyness")
+    ax.set_ylabel("Time to expiry (in years)")
+    ax.set_zlabel("IV")
+    plt.show()
+
+
