7a4ae216242bbc8d7d0db87606cb44b210502f0c
Timotej Dzian <timotejdzian@email.com>
2026-06-06 21:33:36 +0200

    used plotly for rendering

--- bs/visualisation.py
+++ bs/visualisation.py
@@ -1,36 +1,46 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
 from scipy.interpolate import griddata
+import plotly.graph_objects as go
 
 
 def visualize(calls_df, S):
     calls_df = calls_df.copy()
     calls_df["moneyness"] = calls_df["strike"] / S
-    filters = ((calls_df["mid_price"] > 0) 
-              & (calls_df["IV"].between(0.01, 5)) 
-              & (calls_df["T"] >= 5/365) 
-              & (calls_df["moneyness"].between(0.7, 1.3)))
+    filters = (
+        (calls_df["mid_price"] > 0)
+        & (calls_df["IV"].between(0.01, 5))
+        & (calls_df["T"] >= 5 / 365)
+        & (calls_df["moneyness"].between(0.7, 1.3))
+    )
     t_calls_df = calls_df[filters].copy()
-    
+
     x = t_calls_df["moneyness"].values
     y = t_calls_df["T"].values
     z = t_calls_df["IV"].values
 
-    # create 2D space of evenly spaced data 
+    # create 2D space of evenly spaced data
     xi = np.linspace(x.min(), x.max(), 50)
     yi = np.linspace(y.min(), y.max(), 50)
     X, Y = np.meshgrid(xi, yi)
-    # interpolation 
-    Z = griddata((x, y), z, (X, Y), method = "linear")
+    # interpolation
+    Z = griddata((x, y), z, (X, Y), method="linear")
 
-    #create the graph
-    fig = plt.figure(figsize = (12, 9))
-    ax = fig.add_subplot(111, projection="3d")
-    ax.plot_surface(X, Y, Z, cmap = "viridis", edgecolor = "none", alpha = 0.9)
-    ax.set_xlabel("Moneyness")
-    ax.set_ylabel("Time to expiry (in years)")
-    ax.set_zlabel("IV")
-    plt.show()
+    # create the graph
+    fig = go.Figure(data=[go.Surface(
+        x=X, y=Y, z=Z,
+        colorscale="Viridis",
+        opacity=0.9
+    )])
 
+    fig.update_layout(
+        scene=dict(
+            xaxis_title="Moneyness",
+            yaxis_title="Time to expiry (in years)",
+            zaxis_title="IV",
+        ),
+        width=900,
+        height=700,
+    )
 
+    fig.show(renderer="browser")
